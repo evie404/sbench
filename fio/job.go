bd2d399b2f1f7435f43646af7e6ee2ba80953d9f
Ricky Pai <rickyp999+github@gmail.com>
2020-12-25 18:09:34 -0800

    support running job

--- fio/job.go
+++ fio/job.go
@@ -1,5 +1,11 @@
 package fio
 
+import (
+	"io"
+	"os/exec"
+	"strconv"
+)
+
 type Job struct {
 	Name      string
 	Direct    bool
@@ -13,4 +19,44 @@ type Job struct {
 	Loops     int
 	ReadWrite RWAccess
 	Thread    bool
+}
+
+func (j *Job) Run(stdout, stderr io.Writer) error {
+	direct := "0"
+	if j.Direct {
+		direct = "1"
+	}
+
+	thread := "0"
+	if j.Thread {
+		thread = "1"
+	}
+
+	cmd := exec.Command(
+		"fio",
+		"--name="+j.Name,
+		"--direct="+direct,
+		"--iodepth="+strconv.Itoa(j.IODepth),
+		"--numjobs="+strconv.Itoa(j.NumJobs),
+		"--max-jobs="+strconv.Itoa(j.NumJobs),
+		"--rwmixread="+strconv.Itoa(j.RWMixRead),
+		"--ioengine="+string(j.IOEngine),
+		"--blocksize="+j.BlockSize,
+		"--size="+j.Size,
+		"--loops="+strconv.Itoa(j.Loops),
+		"--rw="+string(j.ReadWrite),
+		"--thread="+thread,
+
+		"--parse-only",
+
+		"--directory="+j.Directory,
+	)
+	cmd.Stdout = stdout
+	cmd.Stderr = stderr
+	err := cmd.Run()
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
